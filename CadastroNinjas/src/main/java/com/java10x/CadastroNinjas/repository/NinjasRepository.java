5690c15305faaa8651928481bce77ee55e245cd7
carlos.henrique <carloshenriquesw@gmail.com>
2025-07-19 02:25:23 -0300

    Removendo arquivos ignorados do repositório

--- CadastroNinjas/src/main/java/com/java10x/CadastroNinjas/repository/NinjasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java10x.CadastroNinjas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java10x.CadastroNinjas.model.NinjaModel;
-
-public interface NinjasRepository extends JpaRepository<NinjaModel, Long> {
-
-}
